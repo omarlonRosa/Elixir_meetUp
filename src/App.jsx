4b3dc9359230179391d248e41b91105bb45afbc0
Marlon Rosa <rosa.marlon@icloud.com>
2025-04-28 03:40:27 -0300

    adicionando bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import speakers from "./data/speakers.json";
 
 function App() {
   return (
-    <div className="relative overflow-hidden min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
+    <div className="relative overflow-hidden min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900 bg-cover bg-center" style={{
+      backgroundImage: "url('/backgroundImage.png')"}}>
       <CanvasBackground />
       <div className="z-10 flex flex-col items-center p-4 md:px-6 space-y-8">
         <img src="/logo.svg" alt="Logo Elixir Meetup" className="h-20" />
 
         <Countdown />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 place-items-center">
           {speakers.map((speaker, index) => (
         <PresenterCard key={index} data={speaker} delay={index * 200}/>
         ))}
